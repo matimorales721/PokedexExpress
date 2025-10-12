0802f583399213f93c8c08168a59e4f9d2cea761
Matias Morales <matimorales721@gmail.com>
2025-10-12 01:02:23 -0300

    arreglitos

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,20 @@ import PokemonLista from './pages/PokemonLista.jsx'
 import PokemonDetalle from './pages/PokemonDetalle.jsx'
 import Navbar from './components/Navbar.jsx'
 import imagenDeFondo from './images/pokeball_pattern.jpg'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
   return (
     
-      <div className='Container-App' style={{ backgroundImage: `url(${imagenDeFondo})`, minHeight: '100vh', backgroundSize: 'auto', backgroundRepeat: 'repeat', backgroundPosition: 'left top', width: '100%' }}>
+      <div className='imagen-fondo' style={{ backgroundImage: `url(${imagenDeFondo})`}}>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pokemon" element={<PokemonLista />} />
           <Route path="/pokemon/:name" element={<PokemonDetalle />} />
         </Routes>
-      </div>
-   
+      </div>   
   )
 }
 
